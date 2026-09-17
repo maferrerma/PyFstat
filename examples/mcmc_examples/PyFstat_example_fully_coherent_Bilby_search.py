8f93c25ef095df905f5608afde1642dfc6e99d6e
maferrerma <maria-antonia.ferrer@ligo.org>
2026-09-17 12:24:34 +0200

    Adds new example for running fully coherent search using ptemcee through Bilby.

--- examples/mcmc_examples/PyFstat_example_fully_coherent_Bilby_search.py
+++ examples/mcmc_examples/PyFstat_example_fully_coherent_Bilby_search.py
@@ -10,15 +10,11 @@ This example mirrors the basic fully coherent MCMC example, but calls
 """
 
 import os
-import sys
 
 import numpy as np
 
 import pyfstat
-
-repo_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
-if os.path.isdir(os.path.join(repo_root, "pyfstat")):
-    sys.path.insert(0, repo_root)
+from pyfstat.utils import get_predict_fstat_parameters_from_dict
 
 label = "PyFstatExampleFullyCoherentBilbySearch"
 outdir = os.path.join("PyFstat_example_data", label)
@@ -60,6 +56,10 @@ logger.info("Predicted twoF value: {}\n".format(twoF))
 
 DeltaF0 = 5e-7
 DeltaF1 = 5e-13
+VF0 = (np.pi * data_parameters["duration"] * DeltaF0) ** 2 / 3.0
+VF1 = (np.pi * data_parameters["duration"] ** 2 * DeltaF1) ** 2 * 4 / 45.0
+logger.info("\nV={:1.2e}, VF0={:1.2e}, VF1={:1.2e}\n".format(VF0 * VF1, VF0, VF1))
+
 theta_prior = {
     "F0": {
         "type": "unif",
@@ -71,21 +71,11 @@ theta_prior = {
         "lower": signal_parameters["F1"] - DeltaF1 / 2.0,
         "upper": signal_parameters["F1"] + DeltaF1 / 2.0,
     },
-    "transient_tstart": {
-        "type": "unif",
-        "lower": data.tstart,
-        "upper": data.tstart + data.duration - 2 * data.Tsft,
-    },
-    "transient_duration": {
-        "type": "unif",
-        "lower": 2 * data.Tsft,
-        "upper": data.duration - 2 * data.Tsft,
-    },
 }
 for key in "F2", "Alpha", "Delta":
     theta_prior[key] = signal_parameters[key]
 
-search = pyfstat.MCMCTransientSearch(
+search = pyfstat.MCMCSearch(
     label=label,
     outdir=outdir,
     sftfilepattern=data.sftfilepath,
@@ -100,7 +90,6 @@ search.transform_dictionary = dict(
         subtractor=signal_parameters["F1"], symbol="$\\dot{f}-\\dot{f}^\\mathrm{s}$"
     ),
 )
-
 result = search.run_bilby(
     sampler="dynesty",
     sampler_kwargs={
@@ -111,7 +100,22 @@ result = search.run_bilby(
     save_bilby_logs=True,
     save_bilby_progress=True,
 )
-
 logger.info("Bilby result object: {}".format(result))
 search.print_summary()
 search.plot_corner(add_prior=True, truths=signal_parameters)
+search.plot_prior_posterior(injection_parameters=signal_parameters)
+
+search.generate_loudest()
+
+search.print_summary()
+
+# plot cumulative 2F, first building a dict as required for PredictFStat
+d, maxtwoF = search.get_max_twoF()
+for key, val in search.theta_prior.items():
+    if key not in d:
+        d[key] = val
+d["h0"] = data.h0
+d["cosi"] = data.cosi
+d["psi"] = data.psi
+PFS_input = get_predict_fstat_parameters_from_dict(d)
+search.plot_cumulative_max(PFS_input=PFS_input)
